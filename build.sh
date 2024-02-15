ff4c7faa7ce4642c966db2acfc09eaa55f2d0cd0
cyckl <barista@daniel.cafe>
2024-02-15 12:52:03 -0800

    change build date string to include seconds

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-go build -ldflags "-X main.buildDate=$(date -I) -X main.commit=$(git rev-list HEAD --count)"
+go build -ldflags "-X main.buildDate=$(date -Iseconds) -X main.commit=$(git rev-list HEAD --count)"
